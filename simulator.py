39e2f28f0646600dfadb6c707bae74af9e1ea001
williamnet97 <nester@advancedblockchain.com>
2019-05-05 18:55:18 -0500

    helper functions before main

--- simulator.py
+++ simulator.py
@@ -11,38 +11,6 @@ import cache
 cache_line_size = 64
 offset_bits = int(math.log(64, 2))
 
-######## user interface #########
-parser = argparse.ArgumentParser(description='Cache Simulator.')
-parser.add_argument('-s', '--size', metavar='N', type=str, dest='size',
-                    help='the size of the cache in B, KB or MB', required=True)
-parser.add_argument('-a', '--assoc', dest='assoc', type=int, metavar='s',
-                    help='the set associativity of the cache', required=True)
-parser.add_argument('-f', '--file', metavar='FILENAME', type=str, dest='file',
-                    help="name of the input memory trace file; if the file is "
-                    "in gzip format, the file name must end with .gz",
-                    required=True)
-parser.add_argument('--debug', dest='debug', action='store_const',
-                    const=True, default=False,
-                    help='enable debugging logs')
-parser.add_argument('--print', dest='enable_print', action='store_const',
-                    const=True, default=False,
-                    help='enable cache content output')
-args = parser.parse_args()
-
-if args.debug:
-    logging.basicConfig(level=logging.DEBUG)
-
-cacheSize = parse_size(args.size)
-associativity = 2 ** args.assoc
-
-cache = cache.Cache(cacheSize, cache_line_size, associativity, offset_bits)
-
-trace_file = open(args.file)
-trace = trace_file.read().splitlines()
-trace = [item for item in trace if not item.startswith('#')]
-
-simulate(cache, trace, minLength)
-
 ######## helper functions ########
 
 # parse the user-input size string, returns the size in bytes
@@ -91,3 +59,37 @@ def simulate(cache, trace, minLength):
 def print_results(misses, hits):
     ratio = (hits / ((hits + misses) if misses else 1)) * 100
     print("Hit/Miss Ratio: {0:.2f}%".format(ratio) + "\n")
+
+
+######## user interface #########
+parser = argparse.ArgumentParser(description='Cache Simulator.')
+parser.add_argument('-s', '--size', metavar='N', type=str, dest='size',
+                    help='the size of the cache in B, KB or MB', required=True)
+parser.add_argument('-a', '--assoc', dest='assoc', type=int, metavar='s',
+                    help='the set associativity of the cache', required=True)
+parser.add_argument('-f', '--file', metavar='FILENAME', type=str, dest='file',
+                    help="name of the input memory trace file; if the file is "
+                    "in gzip format, the file name must end with .gz",
+                    required=True)
+parser.add_argument('--debug', dest='debug', action='store_const',
+                    const=True, default=False,
+                    help='enable debugging logs')
+parser.add_argument('--print', dest='enable_print', action='store_const',
+                    const=True, default=False,
+                    help='enable cache content output')
+args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+
+cacheSize = parse_size(args.size)
+associativity = 2 ** args.assoc
+
+cache = cache.Cache(cacheSize, cache_line_size, associativity, offset_bits)
+
+trace_file = open(args.file)
+trace = trace_file.read().splitlines()
+trace = [item for item in trace if not item.startswith('#')]
+
+simulate(cache, trace, minLength)
+
